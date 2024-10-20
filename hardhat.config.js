a0dcf1e8553320aec7fff1bfa4ed345160df7b0a
potvik <yuriy@harmony.one>
2024-10-20 03:03:17 +0300

    added deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,68 @@
-require('dotenv').config();
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-contract-sizer")
+require('@typechain/hardhat')
 
 const {
-  PRIVATE__KEY,
-  PRIVATE_KEY,
-  SEPOLIA_API_URL,
-  MAINNET_API_URL,
-  ETHERSCAN_API_KEY,
-  BASE_API_KEY
-} = process.env;
+  MAINNET_URL,
+  MAINNET_DEPLOY_KEY
+} = require("./env.json")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const getEnvAccounts = (DEFAULT_DEPLOYER_KEY) => {
+  return [DEFAULT_DEPLOYER_KEY];
+};
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners()
+
+  for (const account of accounts) {
+    console.info(account.address)
+  }
+})
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const balance = await ethers.provider.getBalance(taskArgs.account);
+
+    console.log(ethers.utils.formatEther(balance), "ETH");
+  });
+
+task("processFees", "Processes fees")
+  .addParam("steps", "The steps to run")
+  .setAction(async (taskArgs) => {
+    const { processFees } = require("./scripts/core/processFees")
+    await processFees(taskArgs)
+  })
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
+  networks: {
+    localhost: {
+      timeout: 120000
+    },
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    mainnet: {
+      url: MAINNET_URL,
+      accounts: [MAINNET_DEPLOY_KEY],
+      gasLimit: 30000000,
+      gasPrice: 101000000000,
+    }
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: MAINNET_DEPLOY_KEY,
+    }
+  },
   solidity: {
     version: "0.8.24",
     settings: {
@@ -22,40 +73,8 @@ module.exports = {
       viaIR: true
     }
   },
-  defaultNetwork: "sepolia",
-  networks: {
-    hardhat: {},
-    sepolia: {
-      url: `${SEPOLIA_API_URL}`,
-      accounts: [`0x${PRIVATE__KEY}`]
-    },
-    mainnet: {
-      url: `${MAINNET_API_URL}`,
-      accounts: [`0x${PRIVATE_KEY}`]
-    },
-    base_mainnet: {
-      url: 'https://mainnet.base.org',
-      accounts: [`0x${PRIVATE_KEY}`],
-      gasPrice: 1000000000,
-    },
-    base_sepolia: {
-      url: 'https://sepolia.base.org',
-      accounts: [`0x${PRIVATE__KEY}`],
-      gasPrice: 1000000000,
-    }
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
   },
-  paths: {
-    sources: "./contracts",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 40000
-  },
-  etherscan: {
-    apiKey: BASE_API_KEY
-  },
-  sourcify: {
-    enabled: true
-  }
-};
+}
