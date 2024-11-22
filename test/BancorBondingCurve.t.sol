47b842430ce672c2f4dbffd564b7d313ea2a5fb8
polymorpher <aaron@hiddenstate.xyz>
2024-11-22 03:55:13 -0800

    more tests

--- test/BancorBondingCurve.t.sol
+++ test/BancorBondingCurve.t.sol
@@ -14,40 +14,61 @@ contract BancorBondingCurveTest is Test {
         quadraticCurve = new BancorBondingCurve((SLOPE_SCALE * 1) / 2, MAX_WEIGHT / (2 + 1));
     }
 
-    uint256 internal b = 1 ether;
-    uint256 internal supply = 1000 ether;
-    uint256 internal p = 0.5 ether;
-    uint256 internal k = 50 ether;
+    uint256 internal supply = 1e6;
+    uint256 internal p = 10 ether;
+    uint256 internal k = 50000;
 
-    function test_BuySellEquivalence() public view {
-        {
-            uint256 mintingAmount = linearCurve.computeMintingAmountFromPrice(b, supply, p);
-            uint256 burningAmount = linearCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
-            assertEq(mintingAmount, burningAmount, "mintingAmount!=burningAmount");
-            uint256 price = linearCurve.computePriceForMinting(b, supply, k);
-            uint256 refund = linearCurve.computeRefundForBurning(b + price, supply + k, k);
-            assertEq(price, refund, "price!=refund");
-            uint256 refundForMintAmount = linearCurve.computeRefundForBurning(b + p, supply + mintingAmount, mintingAmount);
-            assertApproxEqAbs(p, refundForMintAmount, 1 gwei, "p!=refundForMintAmount");
-            uint256 priceForBurningAmount = linearCurve.computePriceForMinting(b, supply, burningAmount);
-            assertApproxEqAbs(p, priceForBurningAmount, 1 gwei, "p!=priceForBurningAmount");
-        }
-        {
-            uint256 mintingAmount = quadraticCurve.computeMintingAmountFromPrice(b, supply, p);
-            uint256 burningAmount = quadraticCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
-            assertEq(mintingAmount, burningAmount, "mintingAmount!=burningAmount");
-            uint256 price = quadraticCurve.computePriceForMinting(b, supply, k);
-            uint256 refund = quadraticCurve.computeRefundForBurning(b + price, supply + k, k);
-            assertEq(price, refund, "price!=refund");
-            uint256 refundForMintAmount = quadraticCurve.computeRefundForBurning(b + p, supply + mintingAmount, mintingAmount);
-            assertApproxEqAbs(p, refundForMintAmount, 1 gwei, "p!=refundForMintAmount");
-            uint256 priceForBurningAmount = quadraticCurve.computePriceForMinting(b, supply, burningAmount);
-            assertApproxEqAbs(p, priceForBurningAmount, 1 gwei, "p!=priceForBurningAmount");
-        }
+    function test_BuySellEquivalence_Linear() public view {
+
+        uint256 bEstimated = uint256(supply * supply);
+        uint256 b = linearCurve.computePriceForMinting(0, 0, supply);
+//        assertEq(b, bEstimated, "b!=bEstimated");
+        uint256 mintingAmount = linearCurve.computeMintingAmountFromPrice(b, supply, p);
+        uint256 burningAmount = linearCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
+        assertEq(mintingAmount, burningAmount, "mintingAmount!=burningAmount");
+        uint256 price = linearCurve.computePriceForMinting(b, supply, k);
+        uint256 refund = linearCurve.computeRefundForBurning(b + price, supply + k, k);
+        assertEq(price, refund, "price!=refund");
+        uint256 refundForMintAmount = linearCurve.computeRefundForBurning(b + p, supply + mintingAmount, mintingAmount);
+        assertApproxEqRel(p, refundForMintAmount, 0.0001 ether, "p!=refundForMintAmount");
+        uint256 priceForBurningAmount = linearCurve.computePriceForMinting(b, supply, burningAmount);
+        assertApproxEqRel(p, priceForBurningAmount, 0.0001 ether, "p!=priceForBurningAmount");
+
     }
 
-//    function test_linearCurveExpectedValues() public view {
+    function test_BuySellEquivalence_Quadratic() public view {
+        uint256 bEstimated = uint256(supply * supply * supply / uint256(6));
+        uint256 b = quadraticCurve.computePriceForMinting(0, 0, supply);
+//        assertEq(b, bEstimated, "b!=bEstimated");
+        uint256 mintingAmount = quadraticCurve.computeMintingAmountFromPrice(b, supply, p);
+        uint256 burningAmount = quadraticCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
+        assertEq(mintingAmount, burningAmount, "mintingAmount!=burningAmount");
+        uint256 price = quadraticCurve.computePriceForMinting(b, supply, k);
+        uint256 refund = quadraticCurve.computeRefundForBurning(b + price, supply + k, k);
+        assertApproxEqRel(price, refund, 0.0001 ether, "price!=refund");
+        uint256 refundForMintAmount = quadraticCurve.computeRefundForBurning(b + p, supply + mintingAmount, mintingAmount);
+        assertApproxEqRel(p, refundForMintAmount, 0.0001 ether, "p!=refundForMintAmount");
+        uint256 priceForBurningAmount = quadraticCurve.computePriceForMinting(b, supply, burningAmount);
+        assertApproxEqRel(p, priceForBurningAmount, 0.0001 ether, "p!=priceForBurningAmount");
+
+    }
 //
+//    function test_linearCurveExpectedValues() public view {
+//        uint256 b = linearCurve.computePriceForMinting(0, 0, supply);
+//        uint256 price = linearCurve.computePriceForMinting(b, supply, k);
+//        console.log("(supply + k) * (supply + k) = %s", (supply + k) * (supply + k));
+//        console.log("supply * supply", supply * supply);
+//        uint256 integral = ((supply + k) * (supply + k)) - supply * supply;
+//        assertApproxEqRel(price, integral, 0.0001 ether, "price!=integral");
+//    }
+//
+//    function test_quadraticCurveExpectedValues() public view {
+//        uint256 b = quadraticCurve.computePriceForMinting(0, 0, supply);
+//        uint256 price = quadraticCurve.computePriceForMinting(b, supply, k);
+//        console.log("(supply + k) * (supply + k) * (supply + k) / 6 = %s", (supply + k) * (supply + k) * (supply + k) / 6);
+//        console.log("supply * supply * supply / 6 = %s", supply * supply * supply / 6);
+//        uint256 integral = (supply + k) * (supply + k) * (supply + k) / 6 - supply * supply * supply / 6;
+//        assertApproxEqRel(price, integral, 0.0001 ether, "price!=integral");
 //    }
 
 }
