d7412dbc07a93c942adb2fe55879ad83aee7cf24
polymorpher <aaron@qokka.ai>
2024-11-22 00:32:32 -0800

    fix math for computeBurningAmountFromRefund

--- contracts/BancorBondingCurve.sol
+++ contracts/BancorBondingCurve.sol
@@ -2,13 +2,16 @@ pragma solidity ^0.8.26;
 
 import "./BancorFormula.sol";
 
+uint256 constant SLOPE_SCALE = 10000;
+
 // based on https://medium.com/relevant-community/bonding-curves-in-depth-intuition-parametrization-d3905a681e0a
 contract BancorBondingCurve is BancorFormula {
     uint256 public immutable slope;
+
     uint32 public immutable reserveRatio;
 
-    // reserveRatio = connectorWeight, but is scaled by MAX_WEIGHT (1000000)
-    // also note that unscaled reserveRatio = 1 / (n+1), so a reserveRatio 1000000 means n=0, reserveRatio=2000000 means n=1, and so on
+    // reserveRatio = connectorWeight, connectorWeight / MAX_WEIGHT represents  1 / (n+1). Given MAX_WEIGHT=1000000 (used as a scaling factor here), it means connectorWeight  = 1000000 / (n+1)
+    // so for n=1, connectorWeight=500000; n=2, connectorWeight=333333, and so on
     // slope (denoted as m in the article) is only relevant when supply = 0. When supply is non-zero, the price for minting k tokens can be fully determined by current balance and supply
     constructor(uint256 _slope, uint32 _reserveRatio) {
         slope = _slope;
@@ -31,7 +34,7 @@ contract BancorBondingCurve is BancorFormula {
         if (supply == 0) {
             uint256 result;
             uint8 precision;
-            (result, precision) = power(p * MAX_WEIGHT, reserveRatio * slope, reserveRatio, MAX_WEIGHT);
+            (result, precision) = power(p * MAX_WEIGHT * SLOPE_SCALE, reserveRatio * slope, reserveRatio, MAX_WEIGHT);
             return result >> precision;
         }
         return calculatePurchaseReturn(supply, b, reserveRatio, p);
@@ -49,6 +52,8 @@ contract BancorBondingCurve is BancorFormula {
         if (b == p) {
             return supply;
         }
-        return calculatePurchaseReturn(supply, b - p, reserveRatio, p);
+        uint256 k0 = calculatePurchaseReturn(supply, b - p, reserveRatio, p);
+        uint256 k = (k0 * supply) / (k0 + supply);
+        return k;
     }
 }
