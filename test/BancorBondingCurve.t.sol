a664d807b7047c8a1ded5f3d3cdb3bc5a41eb92a
polymorpher <aaron@hiddenstate.xyz>
2024-11-22 02:05:58 -0800

    fix quadratic curve

--- test/BancorBondingCurve.t.sol
+++ test/BancorBondingCurve.t.sol
@@ -11,7 +11,7 @@ contract BancorBondingCurveTest is Test {
         // n=1, m=2
         linearCurve = new BancorBondingCurve(SLOPE_SCALE * 2.0, MAX_WEIGHT / (1 + 1));
         // n=2, m=0.5
-        quadraticCurve = new BancorBondingCurve((SLOPE_SCALE * 1) / 2, MAX_WEIGHT * (2 + 1));
+        quadraticCurve = new BancorBondingCurve((SLOPE_SCALE * 1) / 2, MAX_WEIGHT / (2 + 1));
     }
 
     function test_BuySellEquivalenceLinear() public {
@@ -19,13 +19,24 @@ contract BancorBondingCurveTest is Test {
         uint256 supply = 1000 ether;
         uint256 p = 0.5 ether;
         //        console.log(address(linearCurve));
-        uint256 mintingAmount = linearCurve.computeMintingAmountFromPrice(b, supply, p);
-        uint256 burningAmount = linearCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
-        assertEqUint(mintingAmount, burningAmount);
-        uint256 k = 50 ether;
-        uint256 price = linearCurve.computePriceForMinting(b, supply, k);
-        uint256 refund = linearCurve.computeRefundForBurning(b + price, supply + k, k);
-        assertEqUint(price, refund);
+        {
+            uint256 mintingAmount = linearCurve.computeMintingAmountFromPrice(b, supply, p);
+            uint256 burningAmount = linearCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
+            assertEqUint(mintingAmount, burningAmount);
+            uint256 k = 50 ether;
+            uint256 price = linearCurve.computePriceForMinting(b, supply, k);
+            uint256 refund = linearCurve.computeRefundForBurning(b + price, supply + k, k);
+            assertEqUint(price, refund);
+        }
+        {
+            uint256 mintingAmount = quadraticCurve.computeMintingAmountFromPrice(b, supply, p);
+            uint256 burningAmount = quadraticCurve.computeBurningAmountFromRefund(b + p, supply + mintingAmount, p);
+            assertEqUint(mintingAmount, burningAmount);
+            uint256 k = 50 ether;
+            uint256 price = quadraticCurve.computePriceForMinting(b, supply, k);
+            uint256 refund = quadraticCurve.computeRefundForBurning(b + price, supply + k, k);
+            assertEqUint(price, refund);
+        }
     }
 
 
