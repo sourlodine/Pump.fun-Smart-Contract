99aa30cd464cd0efbc521237f969484540a23618
polymorpher <aaron@hiddenstate.xyz>
2024-11-24 02:10:17 -0800

    Implement Bancor curve using new formula implementation

--- contracts/BancorBondingCurve.sol
+++ contracts/BancorBondingCurve.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.8.26;
+
 import "./BancorFormula.sol";
 
 uint256 constant SLOPE_SCALE = 10000;
+import {UD60x18, ud, convert, unwrap, pow, add, mul, sub, div, convert} from "@prb/math/src/UD60x18.sol";
 
 // based on https://medium.com/relevant-community/bonding-curves-in-depth-intuition-parametrization-d3905a681e0a
 contract BancorBondingCurve is BancorFormula {
@@ -18,41 +20,58 @@ contract BancorBondingCurve is BancorFormula {
     }
 
     // buy function
-    function computePriceForMinting(uint256 b, uint256 supply, uint256 k) public view returns (uint256 p) {
-        if (supply == 0) {
-            uint256 result;
-            uint8 precision;
-            (result, precision) = power(k, 1, MAX_WEIGHT, reserveRatio);
-            return ((slope * reserveRatio * result) / SLOPE_SCALE / MAX_WEIGHT) >> precision;
+    function computePriceForMinting(uint256 b, uint256 s, uint256 k) public view returns (uint256 p) {
+        UD60x18 sw = ud(s);
+        UD60x18 bw = ud(b);
+        UD60x18 kw = ud(k);
+        if (s == 0) {
+            UD60x18 wi = div(convert(MAX_WEIGHT), convert(reserveRatio));
+            UD60x18 r = pow(kw, wi);
+            UD60x18 m = div(convert(slope), convert(SLOPE_SCALE));
+            UD60x18 pw0 = div(mul(r, m), wi);
+            return unwrap(pw0);
         }
-        uint256 pp = calculateSaleReturn(supply + k, b, reserveRatio, k);
-        p = b * pp / (b - pp);
+        UD60x18 ppw = calculateSaleReturn(add(sw, kw), bw, reserveRatio, kw);
+        UD60x18 pw = div(mul(bw, ppw), sub(bw, ppw));
+        p = unwrap(pw);
     }
 
     // buy function
-    function computeMintingAmountFromPrice(uint256 b, uint256 supply, uint256 p) public view returns (uint256 k) {
-        if (supply == 0) {
-            uint256 result;
-            uint8 precision;
-            (result, precision) = power(p * MAX_WEIGHT * SLOPE_SCALE, reserveRatio * slope, reserveRatio, MAX_WEIGHT);
-            return result >> precision;
+    function computeMintingAmountFromPrice(uint256 b, uint256 s, uint256 p) public view returns (uint256 k) {
+        UD60x18 sw = ud(s);
+        UD60x18 bw = ud(b);
+        UD60x18 pw = ud(p);
+        if (s == 0) {
+            UD60x18 ww = div(convert(reserveRatio), convert(MAX_WEIGHT));
+            UD60x18 mw = div(convert(slope), convert(SLOPE_SCALE));
+            UD60x18 base = div(div(pw, ww), mw);
+            UD60x18 kw0 = pow(base, ww);
+            return unwrap(kw0);
         }
-        return calculatePurchaseReturn(supply, b, reserveRatio, p);
+        UD60x18 kw = calculatePurchaseReturn(sw, bw, reserveRatio, pw);
+        k = unwrap(kw);
     }
 
     // sell function
-    function computeRefundForBurning(uint256 b, uint256 supply, uint256 k) public view returns (uint256 p) {
-        if (supply == k) {
+    function computeRefundForBurning(uint256 b, uint256 s, uint256 k) public view returns (uint256 p) {
+        if (s == k) {
             return b;
         }
-        return calculateSaleReturn(supply, b, reserveRatio, k);
+        UD60x18 sw = ud(s);
+        UD60x18 bw = ud(b);
+        UD60x18 kw = ud(k);
+        UD60x18 pw = calculateSaleReturn(sw, bw, reserveRatio, kw);
+        p = unwrap(pw);
     }
 
-    function computeBurningAmountFromRefund(uint256 b, uint256 supply, uint256 p) public view returns (uint256 k) {
+    function computeBurningAmountFromRefund(uint256 b, uint256 s, uint256 p) public view returns (uint256 k) {
         if (b == p) {
-            return supply;
+            return s;
         }
-        uint256 k0 = calculatePurchaseReturn(supply, b - p, reserveRatio, p);
-        k = (k0 * supply) / (k0 + supply);
+        UD60x18 sw = ud(s);
+        UD60x18 bw = ud(b);
+        UD60x18 pw = ud(p);
+        UD60x18 k0w = calculatePurchaseReturn(sw, sub(bw, pw), reserveRatio, pw);
+        k = unwrap(div(mul(k0w, sw), add(k0w, sw)));
     }
 }
