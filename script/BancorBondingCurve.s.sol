99cf9d81bc96335a96f09b593d382422e63c66b8
polymorpher <aaron@hiddenstate.xyz>
2024-11-24 02:12:54 -0800

    suppress issues in BancorBondingCurve.s.sol

--- script/BancorBondingCurve.s.sol
+++ script/BancorBondingCurve.s.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.26;
 import "forge-std/Script.sol";
 import "@contracts/BancorBondingCurve.sol";
 
-contract BancorBondingCurveDetail is Script, Power {
+contract BancorBondingCurveDetail is Script {
     BancorBondingCurve internal curve;
     //    uint256 internal SLOPE = vm.envUint("SLOPE");
     uint256 internal SLOPE_SCALED = vm.envOr("SLOPE_SCALED", uint256(0));
@@ -33,16 +33,16 @@ contract BancorBondingCurveDetail is Script, Power {
         //        p = ((totalMintAmount ** (N + 1)) * SLOPE_SCALED) / (N + 1) / SLOPE_SCALE;
         // totalMintAmount = log(p * SLOPE_SCALE * (N+1) / SLOPE_SCALED) / (N+1)
 
-//        uint256 estimated = ((supply ** (1 + 3)) * SLOPE_SCALED) / (1 + 3) / SLOPE_SCALE;
+        uint256 estimated = ((supply ** (1 + 3)) * SLOPE_SCALED) / (1 + 3) / SLOPE_SCALE;
 
-        uint256 result;
-        uint256 precision;
-        (result, precision) = power(supply , 1, MAX_WEIGHT, WEIGHT_SCALED);
-        uint256 estimated = ((result * SLOPE_SCALED * WEIGHT_SCALED) / MAX_WEIGHT / SLOPE_SCALE) >> precision;
+//        uint256 result;
+//        uint256 precision;
+//        (result, precision) = power(supply , 1, MAX_WEIGHT, WEIGHT_SCALED);
+//        uint256 estimated = ((result * SLOPE_SCALED * WEIGHT_SCALED) / MAX_WEIGHT / SLOPE_SCALE) >> precision;
 
         console.log("diff = %s", int256(estimated) - int256(b));
-        console.log("| precision = %s", int256(precision));
-        console.log("| result = %s", int256(result));
+//        console.log("| precision = %s", int256(precision));
+//        console.log("| result = %s", int256(result));
         console.log("diff% = %e", ((int256(estimated) - int256(b)) * 1 ether) / int256(estimated));
 
     }
@@ -65,12 +65,12 @@ contract BancorBondingCurveDetail is Script, Power {
         console.log("b = %s supply = %s", b, supply);
 
         //        uint256 estimated = ((totalMintAmount ** (N + 1)) * SLOPE) / (N + 1);
-        uint256 result;
-        uint256 precision;
-        (result, precision) = power(totalMintAmount, 1, MAX_WEIGHT, WEIGHT_SCALED);
-        uint256 estimated = ((result * SLOPE_SCALED * WEIGHT_SCALED) / MAX_WEIGHT / SLOPE_SCALE) >> precision;
+//        uint256 result;
+//        uint256 precision;
+//        (result, precision) = power(totalMintAmount, 1, MAX_WEIGHT, WEIGHT_SCALED);
+//        uint256 estimated = ((result * SLOPE_SCALED * WEIGHT_SCALED) / MAX_WEIGHT / SLOPE_SCALE) >> precision;
 
-        //        uint256 estimated = ((totalMintAmount ** (1 + 1)) * SLOPE_SCALED) / (1 + 1) / SLOPE_SCALE;
+        uint256 estimated = ((totalMintAmount ** (1 + 1)) * SLOPE_SCALED) / (1 + 1) / SLOPE_SCALE;
 
         console.log("estimated = %s", int256(estimated));
         //        console.log("| precision = %s", int256(precision));
